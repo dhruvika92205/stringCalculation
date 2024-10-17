25f91a8b0a0427a659efd72a1e783375b05ab9b2
dhruvika92205 <dhruvika92205@gmail.com>
2024-10-17 16:02:22 +0530

    Test 6 passed with error

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ import stringCalculation from './stringCalculation.js';
 
 const app = express(); 
 const calculator =new stringCalculation();
-console.log(calculator.add(''));
-console.log(calculator.add("1"));  
-console.log(calculator.add("1,5"));
-console.log(calculator.add("1\n2,3"));
-console.log(calculator.add("//;\n1;2")); 
-console.log(calculator.add("'1,-2,3'")); 
+try{
+    console.log(calculator.add(''));
+    console.log(calculator.add("1"));  
+    console.log(calculator.add("1,5"));
+    console.log(calculator.add("1\n2,3"));
+    console.log(calculator.add("//;\n1;2")); 
+    console.log(calculator.add("'1,-2,3,-4'")); 
+}catch(e){
+console.log(e);
+}
+
 
 app.listen(3000,()=>{
     console.log(`Server running `);
--- stringCalculation.js
+++ stringCalculation.js
@@ -1,29 +1,36 @@
-class stringCalculation{
+class stringCalculation {
     add(str) {
         if (str === '') return 0;
         let numStr = str;
         let delimiters = ',';
 
-        if(str.startsWith('//')){
-            const delimitersIndex =str.indexOf('\n');
-            delimiters = str.substring(2,delimitersIndex);            
-            numStr=str.substring(delimitersIndex+1);           
-                     }
+        if (str.startsWith('//')) {
+            const delimitersIndex = str.indexOf('\n');
+            delimiters = str.substring(2, delimitersIndex);
+            numStr = str.substring(delimitersIndex + 1);
+        }
 
 
-       numStr = numStr.replace(/\n/g, delimiters); 
+        numStr = numStr.replace(/\n/g, delimiters);
         const numArray = numStr.split(delimiters);
-        console.log(numArray);
-        let sum = 0;
+        let sum = 0, negativeNumbers = [];
         for (const num of numArray) {
             const parsedNum = parseInt(num, 10);
 
-           
+            if (parsedNum < 0) {
+                negativeNumbers.push(parsedNum);
+            } else {
                 sum += parsedNum;
-           
+            }
+
+
         }
-       
- return sum;
+        if (negativeNumbers.length > 0) {
+            throw new Error("negative numbers not allowed: " + negativeNumbers.join(", "));
+
+        }
+
+        return sum;
     }
 }
 
