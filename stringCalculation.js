7772c7c8e2693e76402b87119cd254ee862dcf01
dhruvika92205 <dhruvika92205@gmail.com>
2024-10-17 15:42:31 +0530

    pass 1,2,3 test case

--- stringCalculation.js
+++ stringCalculation.js
@@ -1,6 +1,21 @@
 class stringCalculation{
     add(str) {
         if (str === '') return 0;
+        let numStr = str;
+        let delimiters = ',';
+
+        const numArray = numStr.split(delimiters);
+        console.log(numArray);
+        let sum = 0;
+        for (const num of numArray) {
+            const parsedNum = parseInt(num, 10);
+
+           
+                sum += parsedNum;
+           
+        }
+       
+ return sum;
     }
 }
 
